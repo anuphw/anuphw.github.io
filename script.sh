ffff3cbe37263e9495ed0e9ab21fcafe629b5ffc
Anup Walvekar <anuphw@gmail.com>
2018-02-24 20:05:18 +0800

    script

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get upgrade
-
+www_home=/home/pi/www
 
 # If you don't have apache installed install it
 if ! [ -x $(which apache2)  ] ; then 
@@ -18,9 +18,9 @@ if [ -x $(which apache2)  ] ; then
 	wget "https://anuphw.github.io/index.php"  -O /tmp/index.php
 	cp  /tmp/apache2.conf /etc/apache2/apache2.conf
 	cp /tmp/000-default.conf /etc/apache2/sites-available/000-default.conf
-	mkdir -p /home/pi/www
-	cp /tmp/index.php /home/pi/www
-	chmod -R 777 /home/pi/www
+	mkdir -p "$www_home"
+	cp /tmp/index.php "$www_home"
+	chmod -R 777 "$www_home"
 	service apache2 restart
 else 
 	echo "Apache is not installed. Run 'sudo apt-get install apache2'"
@@ -33,8 +33,13 @@ if ! [ -x $(which php) ] ; then
 	exit 1
 fi
 
+# Download website contents 
 
+wget "https://www.dropbox.com/s/03544uihvjr0a26/VideoCoding.zip?dl=0" -O /tmp/VideoCoding.zip
+unzip /tmp/VideoCoding.zip -d "$www_home"
+chmod -R 777 "$www_home"
 
 # Find IP addess
 
 echo "Your IP" $(ifconfig | grep inet | grep broadcast | sed 's/netmask.*//;s/inet //g')
+
